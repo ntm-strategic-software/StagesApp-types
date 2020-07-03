485c19927220ea68fdb7450c8bc53ae399048f8a
rBurgett <ryan@burgettweb.net>
2020-07-03 12:38:01 -0400

    Added Question to exports

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports.GeneralRecording = require('./types/general-recording');
 module.exports.Marker = require('./types/marker');
 module.exports.OutlineItem = require('./types/outline-item');
 module.exports.Person = require('./types/person');
+module.exports.Question = require('./types/question');
 module.exports.SALocation = require('./types/sa-location');
 module.exports.GeneralPhoto = require('./types/general-photo');
 module.exports.User = require('./types/user');
