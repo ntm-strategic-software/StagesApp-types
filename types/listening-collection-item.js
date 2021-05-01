3faacf81d83e1bd76d01810c28c9b9a1aacc696a
rBurgett <ryan@burgettdev.net>
2021-05-01 01:44:26 -0400

    Updated litening collection item

--- types/listening-collection-item.js
+++ types/listening-collection-item.js
@@ -19,14 +19,38 @@ class ListeningCollectionItem {
   order = 0;
 
   /**
+   * Audio recording file names
+   * @type {string[]}
+   */
+  recordingsAudio = [];
+
+  /**
+   * Video recording file names
+   * @type {string[]}
+   */
+  recordingsVideo = [];
+
+  /**
+   * Order of recordings by file name
+   * @type {string[]}
+   */
+  recordingsOrder = [];
+
+  /**
    * Title of the item; matches a CE's title
    * @type {string}
    */
   title = '';
 
   /**
+   * User ID
+   * @type {string}
+   */
+  user = '';
+
+  /**
    * Creates a Listening Collection Item Object
-   * @param {{_id: string, listenedToPrev: string[], order: number, title: string}} data
+   * @param {{_id: string, listenedToPrev: string[], order: number, recordingsAudio: string[], recordingsVideo: string[], recordingsOrder: string[], title: string, user: string}} data
    */
   constructor(data = {}) {
     for(const key of Object.keys(data)) {
@@ -36,7 +60,7 @@ class ListeningCollectionItem {
 
   /**
    * Creates an updated Listening Collection Item Object
-   * @param {{_id: string, listenedToPrev: string[], order: number, title: string}} data
+   * @param {{_id: string, listenedToPrev: string[], order: number, recordingsAudio: string[], recordingsVideo: string[], recordingsOrder: string[], title: string, user: string}} data
    * @returns {ListeningCollectionItem}
    */
   set(data) {
