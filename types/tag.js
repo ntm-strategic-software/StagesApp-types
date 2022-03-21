7c54f558f2a20bbb93cb09dcd1d1c6309eb201bb
davidburson <davidburson@users.noreply.github.com>
2022-03-21 17:44:28 -0400

    t281 add Tag table (#19)
    Co-authored-by: davidburson <6555661+davidburson@users.noreply.github.com>

---
+++ types/tag.js
@@ -0,0 +1,43 @@
+/**
+ * Class representing a tag
+ */
+class Tag {
+
+  /**
+   * Unique ID for the tag
+   * @type {string}
+   * @default ''
+   */
+  _id = '';
+
+  /**
+   * Text of the tag
+   * @type {string}
+   */
+  tag = '';
+
+  /**
+   * Creates a tag
+   * @param {Object} data
+   */
+  constructor(data = {}) {
+    for(const key of Object.keys(data)) {
+      this[key] = data[key];
+    }
+  }
+
+  /**
+   * Creates an updated tag
+   * @param {Object} data
+   * @returns {Tag}
+   */
+  set(data) {
+    return new Tag({
+      ...this,
+      ...data
+    });
+  }
+
+}
+
+module.exports = Tag;
