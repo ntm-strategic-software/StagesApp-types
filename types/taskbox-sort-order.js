af389b065fa96901aaada5019af8a511f863b3d8
davidburson <davidburson@users.noreply.github.com>
2022-07-07 23:55:04 -0400

    change TaskBoxSortOrder to include all taskboxes in one row

--- types/taskbox-sort-order.js
+++ types/taskbox-sort-order.js
@@ -1,21 +1,69 @@
 /**
- * Class representing the order tasks should be displayed in a TaskBox
+ * Class representing the order tasks should be displayed in all TaskBoxes.  Since completing a Task causes
+ * the associated ActivityPlan._id to be removed from one TaskBox and added to another TaskBox, it seems best
+ * to keep all TaskBoxes together in a single doc, to prevent TaskBoxes from getting out of sync.
+ *
+ * So, the table represented by this class will have only one row.
+ *
+ * Each Task Box has an array of activityPlanId's, in the order to display the activityPlan's next task.
+ * This works because an activity plan can never have more than one next task, so an activity plan can only
+ * exist once in this table.  When the final task for an activity plan is completed, the activity plan id is
+ * removed from this table.
  */
 class TaskBoxSortOrder {
+  // /**
+  //  * Key of the taskBox.  E.g, 'Plan', 'Process', 'Community', or 'Helper'.  (Playlist sort order is handled differently.)
+  //  * @type {string}
+  //  * @default ''
+  //  */
+  // taskBoxKey = '';
+  //
+  // /**
+  //  * Array of IDs of ActivityPlans, in the sorted order.
+  //  * For each ActivityPlanId, show that ActivityPlan's next task (the task indicated by activityPlan.nextTaskIndex).
+  //  * @type {string[]}
+  //  * @default []
+  //  */
+  // activityPlanIds = [];
+
   /**
-   * Key of the taskBox.  E.g, 'Plan', 'Process', 'Community', or 'Helper'.  (Playlist sort order is handled differently.)
+   * Unique ID.  There will be only one record, but I include the _id for convenience when using methods that require an _id.
    * @type {string}
    * @default ''
    */
-  taskBoxKey = '';
+  _id = '';
 
   /**
-   * Array of IDs of ActivityPlans, in the sorted order.
+   * Plan sort order:  array of IDs of ActivityPlans, in the sorted order.
    * For each ActivityPlanId, show that ActivityPlan's next task (the task indicated by activityPlan.nextTaskIndex).
    * @type {string[]}
    * @default []
    */
-  activityPlanIds = [];
+  Plan = [];
+
+  /**
+   * Process sort order:  array of IDs of ActivityPlans, in the sorted order.
+   * For each ActivityPlanId, show that ActivityPlan's next task (the task indicated by activityPlan.nextTaskIndex).
+   * @type {string[]}
+   * @default []
+   */
+  Process = [];
+
+  /**
+   * Community sort order:  array of IDs of ActivityPlans, in the sorted order.
+   * For each ActivityPlanId, show that ActivityPlan's next task (the task indicated by activityPlan.nextTaskIndex).
+   * @type {string[]}
+   * @default []
+   */
+  Community = [];
+
+  /**
+   * Helper sort order:  array of IDs of ActivityPlans, in the sorted order.
+   * For each ActivityPlanId, show that ActivityPlan's next task (the task indicated by activityPlan.nextTaskIndex).
+   * @type {string[]}
+   * @default []
+   */
+  Helper = [];
 
   /**
    * Creates a TaskBoxSortOrder object
