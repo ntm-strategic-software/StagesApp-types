3e92c371b3c7a7fbb99f20f32ef0ff85b4737ddd
rBurgett <ryan@burgettdev.net>
2023-08-18 20:52:17 -0400

    Moved transcription

--- src/legacy/transcription.ts
+++ src/legacy/transcription.ts
@@ -1,102 +1,98 @@
+import { Transcription as TranscriptionInterface, transcriptionDefaults } from '../transcription';
+
 /**
  * Class representing the transcription of a section of a CultureEvent.  A single Transcription may cross GeneralRecording boundaries.
  * Translation and metadata can be associated with the transcription.
  */
-class Transcription {
+export class Transcription implements TranscriptionInterface {
 
   /**
    * Unique ID for the Transcription
-   * @type {string}
-   * @default ''
    */
-  _id = '';
+  _id: string;
 
   /**
    * Optional title the user may give to this Transcription
-   * @type {string}
    */
-  title = '';
+  title: string;
 
   /**
    * Array of SearchWord._id's
-   * @type {string[]}
    */
-  searchWords = [];
+  searchWords: string[];
 
   /**
    * Array of strings from Tag.tagText
-   * @type {string[]}
    */
-  tags = [];
+  tags: string[];
 
   /**
    * Array of gridItem keys from CultureGrid on Desktop, in the form or row key, hyphen, column key.  Example:  causeEffect–kinship
-   * @type {string[]}
    */
-  gridItems = [];
+  gridItems: string[];
 
   /**
    * The transcribed text.  This is expected to be in the orthography of the target language.
-   * @type {string}
    */
-  text = '';
+  text: string;
 
   /**
    * A translation of Transcription.text into the user's language
-   * @type {string}
    */
-  translation = '';
+  translation: string;
 
   /**
    * Any note the user wants to make about this Transcription
-   * @type {string}
    */
-  note = '';
+  note: string;
 
   /**
    * _id of the CultureEvent this Transcription is a part of
-   * @type {string}
    */
-  cultureEvent = '';
+  cultureEvent: string;
 
   /**
    * The order of this Transcription in relation to all other Transcriptions for this CultureEvent
-   * @type {number}
    */
-  idx = 0;
+  idx: number;
 
   /**
    * Starting time of the transcription, in seconds (with decimals), from the beginning of the CultureEvent.
    * If there are multiple GeneralRecordings in the CultureEvent, this is the time from the beginning of the first GeneralRecording.
-   * @type {number}
    */
-  transcriptionStartTime = 0;
+  transcriptionStartTime: number;
 
   /**
    * see gh216 in desktop.  Per Bill, we don't need this, but since we were already calculating it, I'm leaving it in
    *  just in case we want to use it later.
    *  Ending time of the transcription, in seconds (with decimals), from the beginning of the CultureEvent.
    *  Currently, this is the ending time of the last loop used when creating/editing the text of transcription.
-   * @type {number}
    */
-  transcriptionEndTime = 0;
+  transcriptionEndTime: number;
 
   /**
    * Constructs a Transcription object
-   * @param {Transcription|Object} data
    */
-  constructor(data = {}) {
-    for(const key of Object.keys(data)) {
-      this[key] = data[key];
-    }
+  constructor(data?: TranscriptionInterface) {
+    const defaults = transcriptionDefaults();
+    this._id = data?._id || defaults._id;
+    this.title = data?.title || defaults.title;
+    this.searchWords = data?.searchWords || defaults.searchWords;
+    this.tags = data?.tags || defaults.tags;
+    this.gridItems = data?.gridItems || defaults.gridItems;
+    this.text = data?.text || defaults.text;
+    this.translation = data?.translation || defaults.translation;
+    this.note = data?.note || defaults.note;
+    this.cultureEvent = data?.cultureEvent || defaults.cultureEvent;
+    this.idx = data?.idx || defaults.idx;
+    this.transcriptionStartTime = data?.transcriptionStartTime || defaults.transcriptionStartTime;
+    this.transcriptionEndTime = data?.transcriptionEndTime || defaults.transcriptionEndTime;
   }
 
   /**
    * Creates an updated Transcription object
-   * @param {Transcription|Object} data
-   * @returns {Transcription}
    */
-  set(data) {
+  set(data: Partial<TranscriptionInterface>) {
     return new Transcription({
       ...this,
       ...data,
@@ -104,5 +100,3 @@ class Transcription {
   }
 
 }
-
-module.exports = Transcription;
