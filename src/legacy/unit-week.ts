f4fe8fa35efa548615d9946f62ab6651c83a59a6
davidburson <davidburson@users.noreply.github.com>
2024-11-04 18:05:12 -0500

    Desktop gh802 add table UnitWeeks (#82)
    * desktop-gh802 add table UnitWeeks
    
    * desktop-gh802 add table UnitWeeks

---
+++ src/legacy/unit-week.ts
@@ -0,0 +1,57 @@
+import { UnitWeek as UnitWeekInterface, unitWeekDefault } from '../unit-week';
+import isNumber from 'lodash/isNumber';
+
+/**
+ * Class representing when a user began a week of CLA for a specific unit
+ */
+export class UnitWeek implements UnitWeekInterface {
+
+  /**
+   * Unique ID for the unitWeek entry
+   */
+  _id: string;
+
+  /**
+   * The overall CLA unit the user was in when he began this week of CLA (see User.claUnit)
+   */
+  claUnit: number|null;
+
+  /**
+   * ISO Date (e.g., '2022-06-20T15:50:40.055Z'), when the week of CLA began
+   */
+  weekStart: string;
+
+  /**
+   * ISO Date (e.g., '2022-06-20T15:50:40.055Z'), when the row was initially saved to the database
+   */
+  createdAt?: string;
+
+  /**
+   * ISO Date (e.g., '2022-06-20T15:50:40.055Z'), when the row was last updated in the database
+   */
+  updatedAt?: string;
+
+  /**
+   * Creates a unitWeek entry object
+   * @param {UnitWeek|Object} data
+   */
+  constructor(data?: UnitWeekInterface) {
+    const defaults = unitWeekDefault();
+    this._id = data?._id || defaults._id;
+    const claUnit = data?.claUnit;
+    this.claUnit = isNumber(claUnit) ? claUnit : defaults.claUnit;
+    this.weekStart = data?.weekStart || defaults.weekStart;
+    this.createdAt = data?.createdAt || defaults.createdAt;
+    this.updatedAt = data?.updatedAt || defaults.updatedAt;
+  }
+
+  /**
+   * Creates an updated unitWeek entry object
+   */
+  set(data: Partial<UnitWeekInterface>) {
+    return new UnitWeek({
+      ...this,
+      ...data,
+    });
+  }
+}
